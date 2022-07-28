8ee712a969ea0094a915344e5807ce4842997f36
Subhradeep Sarkar <110169531+Tyr372001@users.noreply.github.com>
2022-07-28 22:44:07 +0530

    Update Complex.java

--- Complex.java
+++ Complex.java
@@ -1,15 +1,16 @@
+// A Simple Java Program to Implement Complex Nmumber and its basic opertations
 public class Complex{
+	// Defining two integers as real and imag
+	private int real;	// real store real part of the complex number
+	private int imag;	// imag store imaginary part of the complex number
 
-	private int real;
-	private int imag;
-
-	Complex()
+	Complex()		// using default constructor to initialise the data
 	{real=0; imag=0;}
 
-	Complex(int real, int imag)
-	{this.real=real; this.imag=imag;}
+	Complex(int real, int imag)		// using parameterised constructor to pass arguments 
+	{this.real=real; this.imag=imag;}	// using this keyword to hide my instance variable and the local variable as they have the same name
 
-	public Complex add(Complex c){
+	public Complex add(Complex c){		// add() method to add two complex number
 
 		Complex res = new Complex();
 
@@ -19,7 +20,7 @@ public class Complex{
 		return res;
 	}
 
-	public Complex subtract(Complex c){
+	public Complex subtract(Complex c){	//	subtract() method to subtract two complex numbers
 
 		Complex res = new Complex();
 
@@ -29,7 +30,7 @@ public class Complex{
 		return res;
 	}
 
-	public Complex multiply(Complex c){
+	public Complex multiply(Complex c){	// multiply() to multiply the complex numbers 
 
 		Complex res = new Complex();
 
@@ -39,11 +40,11 @@ public class Complex{
 		return res;
 	}
 
-	public String toString()
+	public String toString()		// overriding the toString() method to print complex numbers by using print() or println()
 	{
 		return(real + " + "+ imag+"i");
 	}
-
+	// the main() method 
 	public static void main(String[] args) {
 		
 		Complex c1 = new Complex(5,4);
